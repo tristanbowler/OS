8cd70f64d1e8252cd7258f646caf1c6f85d0e3dd
Tristan Bowler <tristan.bowler@gmail.com>
2019-04-16 11:44:48 -0600

    Missed my changes to sleepy.h

--- Assignment5/sleepy.h
+++ Assignment5/sleepy.h
@@ -14,11 +14,13 @@
  *  block_size - maximum number of bytes that can be read or written 
  *    in one call;
  *  sleepy_mutex - a mutex to protect the fields of this structure;
- *  cdev - ñharacter device structure.
+ *  cdev - ï¿½haracter device structure.
  */
 struct sleepy_dev {
-  unsigned char *data;
+  //unsigned char *data; //dead code
   struct mutex sleepy_mutex; 
   struct cdev cdev;
+  wait_queue_head_t queue; 
+  int flag; 
 };
 #endif /* SLEEPY_H_1727_INCLUDED */
